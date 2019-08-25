4e79713c2d8788c9874b9f308f83e09a934731e7
swd-turner <sean.turner@pnnl.gov>
2019-08-25 14:36:19 -0700

    Implement teleconnection count function, including: - i/o functions for getting all cities and ucs power plants - additions to existing i/o functions for method flexibility (rgdal, sp, sf) - projection as a named package constant constants.R

---
+++ R/constants.R
@@ -0,0 +1,3 @@
+
+# projection
+proj4_string <- "+proj=longlat +datum=WGS84 +no_defs"
---
+++ R/count_teleconnections.R
@@ -0,0 +1,80 @@
+#' count_watershed_teleconnections
+#'
+#' @param data_dir root directory for the spatial data ("/pic/projects/im3/teleconnections/data/")
+#' @param cities a vector of cities to be included in the count. If omitted, all cities will be included.
+#' @details counts teleconnections assoicated with water supply catchments associated with each city
+#' @importFrom purrr map_dfr
+#' @importFrom dplyr filter
+#' @export
+#'
+count_watershed_teleconnections <- function(data_dir, cities = NULL){
+
+  all_cities <- get_cities()[["city_state"]]
+
+  # use all cities if "cities" argument is omitted
+  if(is.null(cities)) cities <- all_cities %>% unique()
+
+  # throw error if any chosen city lies outside available cities
+  if(any(!cities %in% all_cities)) {
+    cities[!cities %in% all_cities] -> bad_cities
+    stop(paste0(paste(bad_cities), ": not part of '
+                teleconnect'!"))
+  }
+
+  get_cities() %>%
+    subset(city_state %in% cities) ->
+    city_watershed_mapping
+
+  # read shapefiles for watersheds
+  import_shapefile(paste0(data_dir, "water/CWM_v2_2/World_Watershed8.shp"),
+                   method = "rgdal") %>%
+    # subset for desired watersheds
+    subset(DVSN_ID %in% city_watershed_mapping$DVSN_ID) ->
+    # remove City_Name; it's inconsistent with other shapefiles
+    #dplyr::select(-City_Name) ->
+    watersheds
+
+  # read ucs plant data
+  get_ucs_power_plants(data_dir) -> power_plants_USA
+
+  # map through all cities, computing teleconnections
+  cities %>%
+    map_dfr(function(city){
+      filter(city_watershed_mapping, city_state == !!city) %>%
+        .[["DVSN_ID"]] -> city_intake_ids
+
+      # subset watersheds shapefile for target city
+      watersheds %>%
+        subset(DVSN_ID %in% city_intake_ids) ->
+        watersheds_city
+
+      # TELECONNECTION - NUMBER OF WATERSHEDS
+      tc_n_watersheds <- length(city_intake_ids)
+      # NOTE: CURRENTLY COUNTS NESTED WATERSHEDS; ADDITIONAL...
+      # ... ALGORITHM NEEDED TO AVOID DOUBLE COUNTING
+
+      # subset power plants for target city watersheds
+      power_plants_USA[watersheds_city, ] -> power_plants_city
+
+      # TELECONNECTION - NUMBER OF HYDRO PLANTS
+      power_plants_city %>%
+        subset(`Power Plant Type` == "Hydropower") %>%
+        nrow() ->
+        tc_n_hydroplants
+
+      # TELECONNECTION - NUMBER OF THERMAL PLANTS
+      power_plants_city %>% subset(cooling == "Yes") %>%
+        nrow() ->
+        tc_n_thermalplants
+
+      done(city)
+
+      # output
+      return(
+        tibble(city = !! city,
+               n_watersheds = tc_n_watersheds,
+               n_hydro = tc_n_hydroplants,
+               n_thermal = tc_n_watersheds)
+      )
+    })
+}
