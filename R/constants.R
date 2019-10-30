8cf050708884f130471ec9335aa5995ca019f814
Nelson <kristian.nelson@pnnl.gov>
2019-10-30 14:58:36 -0700

    Edit constants.R

--- R/constants.R
+++ R/constants.R
@@ -1,3 +1,8 @@
 
 # projection
 proj4_string <- "+proj=longlat +datum=WGS84 +no_defs"
+
+# non land CDL classes
+non_land_cdl_classes <- c(0, 81, 83, 88, 111)
+
+non_devcrop_class <- c(0,63:65,81,83:92,111,112,131:195)
