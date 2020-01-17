31c88891afb92983c08a26599da4eb69b669e5c5
Nelson <kristian.nelson@pnnl.gov>
2020-01-17 15:30:14 -0800

    delete splicer cities. add variables.

--- R/constants.R
+++ R/constants.R
@@ -7,7 +7,6 @@ non_land_cdl_classes <- c(0, 81, 83, 88, 111)
 
 non_devcrop_class <- c(0,63:65,81,83:92,111,112,131:195)
 
-# cities with large watersheds
-sliced_cities <- c("New Orleans | LA", "Saint Louis | MO", "Laredo | TX", "Saginaw | MI",
-                   "Flint | MI", "Cleveland | OH","Buffalo | NY", "Detroit | MI",
-                   "Harlingen | TX", "Kansas City | MO", "McAllen | TX", "Sioux City | IA", "Toledo | OH")
+hydro_variable <- "Normal_Storage"
+
+acrefeet_conv <- 1.2335e-6
