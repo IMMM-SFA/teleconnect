7f3a1f55da1601bcb07b438c8a87557e7e4e6cf1
crvernon <chrisrvernon@gmail.com>
2021-02-03 16:25:14 -0500

    use sf sample shapefile for tests

--- tests/testthat/test-analysis.R
+++ tests/testthat/test-analysis.R
@@ -1,43 +1,38 @@
 context("Analysis functions")
 
 
-#' Create a sample polygon sf object to use for tests
-#'
-#' @return sf polygon object
-#' @importFrom sf st_polygon
-#'
-#' @author Chris R. Vernon (chris.vernon@pnnl.gov)
-sample_polygon <- function() {
-
-  # create a sample polygon to use for tests
-  outer = matrix(c(0,0,10,0,10,10,0,10,0,0),ncol=2, byrow=TRUE)
-  hole1 = matrix(c(1,1,1,2,2,2,2,1,1,1),ncol=2, byrow=TRUE)
-  hole2 = matrix(c(5,5,5,6,6,6,6,5,5,5),ncol=2, byrow=TRUE)
-  pts = list(outer, hole1, hole2)
-
-  return(pl1 = st_polygon(pts))
-
-}
-
-
 test_that("init_bbox() functionality", {
 
-  # build sample polygon sf object
-  ply <- sample_polygon()
+  # load sample shapefile from "sf"
+  ply <- sf::st_read(system.file("shape/nc.shp", package="sf"))
 
   # build what to expect as an output
   comp_bbox <- list()
-  comp_bbox[[ "xmin" ]] <- -1
-  comp_bbox[[ "ymin" ]] <- -1
-  comp_bbox[[ "ymax" ]] <- 11
-  comp_bbox[[ "xmin_source" ]] <- 0
-  comp_bbox[[ "xmax_source" ]] <- 10
+  comp_bbox[[ "xmin" ]] <- -84.59462
+  comp_bbox[[ "ymin" ]] <- 33.61123
+  comp_bbox[[ "ymax" ]] <- 36.86041
+  comp_bbox[[ "xmin_source" ]] <- -84.32385
+  comp_bbox[[ "xmax_source" ]] <- -75.45698
 
   # run the function and return the output list
   sim_bbox <- init_bbox(ply)
 
   # ensure that the function produces what is expected
-  expect_equal(comp_bbox, sim_bbox)
+  expect_equal(comp_bbox, sim_bbox, tolerance=1e-5)
+
+})
+
+
+test_that("poly_intersect() functionality", {
+
+  # build sample polygon sf object
+  ply <- sample_polygon()
+
+  # create a bounding box attribute list
+  sim_bbox <- init_bbox(ply)
+
+
+
 
 })
 
